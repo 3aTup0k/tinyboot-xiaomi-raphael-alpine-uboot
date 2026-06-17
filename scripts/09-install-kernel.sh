5f8b77064774cc34bc2d707c47281fbd9298752b
voxelsy/// <145951985+3aTup0k@users.noreply.github.com>
2026-06-17 17:36:01 +1000

    Add files via upload

--- scripts/09-install-kernel.sh
+++ scripts/09-install-kernel.sh
@@ -9,6 +9,19 @@ echo "[$(date +'%Y-%m-%d %H:%M:%S')] [09]   └─ Kernel package directory: ${K
 
 cp ${KERNEL_DEBS_DIR}/*-xiaomi-raphael.deb rootdir/tmp/
 
+#!/bin/bash
+set -e
+
+KERNEL_DEBS_DIR="${KERNEL_DEBS_DIR:-.}"
+
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] [09] 🧠 Installing kernel"
+
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] [09]   └─ Kernel package directory: ${KERNEL_DEBS_DIR}"
+
+# Copy packages to tmp inside rootdir to ensure dpkg can find them
+mkdir -p rootdir/tmp
+cp ${KERNEL_DEBS_DIR}/*-xiaomi-raphael.deb rootdir/tmp/
+
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] [09]   └─ Installing linux-image..."
 chroot rootdir dpkg -i /tmp/linux-image-xiaomi-raphael.deb
 
@@ -18,8 +31,12 @@ chroot rootdir dpkg -i /tmp/linux-headers-xiaomi-raphael.deb
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] [09]   └─ Installing firmware..."
 chroot rootdir dpkg -i /tmp/firmware-xiaomi-raphael.deb
 
+rm -f rootdir/tmp/*-xiaomi-raphael.deb
+
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] [09]   └─ Updating initramfs..."
-chroot rootdir /usr/bin/env PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin /usr/sbin/update-initramfs -c -k all
+chroot rootdir update-initramfs -c -k all
+
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] [09] ✅ Kernel installation completed"
 
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] [09] ✅ Kernel installation completed"
